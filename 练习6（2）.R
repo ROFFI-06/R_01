eaf665e5e8b1a08870f38813768f40ff10887edb
ROFFI-06 <73463381+ROFFI-06@users.noreply.github.com>
2020-11-06 09:49:37 +0800

    Add files via upload

---
+++ 练习6（2）.R
@@ -0,0 +1,19 @@
+multiples_of_pi <- new.env()
+multiples_of_pi[["two_pi"]] <- c(2*pi)
+multiples_of_pi$three_pi <- 3*pi
+multiples_of_pi
+assign(
+  "four_pi",
+  4*pi,
+  multiples_of_pi
+)
+
+
+hypotenuse <- function(x){
+  return(TURE,FALSE)
+}
+hypotenuse(x)
+
+
+hypotenuse <- function(x)
+  return(args,body)
